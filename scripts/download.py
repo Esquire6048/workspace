f683f0e4b0ff900f45d9ea0ba8df7255fd233e65
Bolero Xu <22206837+Esquire6048@users.noreply.github.com>
2024-06-19 06:50:47 +0000

    Make it more readable

--- scripts/download.py
+++ scripts/download.py
@@ -1,11 +1,16 @@
 from typing import Optional
 
 
-def download_from_hub(repo_id: Optional[str] = None, local_dir: str = None, token: str = None) -> None:
+def download_from_hub(repo_id: Optional[str] = None, 
+                      local_dir: str = None, 
+                      token: str = None) -> None:
 
     from huggingface_hub import snapshot_download
 
-    snapshot_download(repo_id=repo_id, local_dir=local_dir, token=token, local_dir_use_symlinks=False)
+    snapshot_download(repo_id=repo_id,
+                      local_dir=local_dir,
+                      token=token,
+                      local_dir_use_symlinks=False)
 
 
 if __name__ == "__main__":
